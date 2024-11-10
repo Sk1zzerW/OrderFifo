d241fe062c062cd9ad07110546760b2efbe67fe1
olegk <kbryzhenyukpc@gmail.com>
2024-11-10 18:19:01 +0200

    Добавьте файлы проекта.

---
+++ OrderFifo/OrderFifo.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <stdexcept>
+
+template <typename T>
+class Queue {
+private:
+    struct Node {
+        T data;
+        Node* next;
+        Node(T value) : data(value), next(nullptr) {}
+    };
+    Node* front;
+    Node* rear;
+    int size;
+
+public:
+    Queue() : front(nullptr), rear(nullptr), size(0) {}
+
+    void enqueue(T value) {
+        Node* newNode = new Node(value);
+        if (rear == nullptr) {
+            front = rear = newNode;
+        }
+        else {
+            rear->next = newNode;
+            rear = newNode;
+        }
+        ++size;
+    }
+
+    void dequeue() {
+        if (isEmpty()) {
+            throw std::out_of_range("Очередь пуста");
+        }
+        Node* temp = front;
+        front = front->next;
+        if (front == nullptr) {
+            rear = nullptr;
+        }
+        delete temp;
+        --size;
+    }
+
+    T peek() const {
+        if (isEmpty()) {
+            throw std::out_of_range("Очередь пуста");
+        }
+        return front->data;
+    }
+
+    bool isEmpty() const {
+        return front == nullptr;
+    }
+
+    int getSize() const {
+        return size;
+    }
+
+    ~Queue() {
+        while (!isEmpty()) {
+            dequeue();
+        }
+    }
+};
